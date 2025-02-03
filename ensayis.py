900b35d102a68f92286154acfe54a6de06ab3db2
Your Name <you@example.com>
2025-02-03 15:22:15 -0500

    Se añaden funciones para practicar la funcionalidad de git y github

--- ensayis.py
+++ ensayis.py
@@ -1,1 +1,6 @@
-print("Hola mundo")
+def Hello_world(nombre:str)->str:
+    saludo = print(f'Hello world im {nombre}!')
+    return saludo
+
+ingreso = input("Ingresa tu nombre: ")
+Hello_world(ingreso)
---
+++ ensayis2.py
@@ -0,0 +1,18 @@
+def Concatenar_Lista(lista:list)->str:
+    i=0
+    concatena =""
+    for i in range(len(lista)):
+        concatena = concatena + lista[i]
+        i+=1
+
+    return concatena
+
+tamano_lista = int(input("Ingrese el tamaño que desea en la lista: "))
+lista = []
+
+for i in range(tamano_lista):
+    elemento = input(f'Ingrese el valor que desea en la posicion [{i}]:  ')
+    lista.append(elemento)
+    i+=1
+
+print(f'Esta es la concatenacion:  {Concatenar_Lista(lista)}')
